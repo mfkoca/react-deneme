104b659cff2e1a32adbfb53ce0237c46fd3aeae4
mfkoca <mehmedfatihkoca@gmail.com>
2021-12-06 16:57:17 +0300

    ...

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Element from './component/FormElementi';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Element> </Element>
+    </div>
+  );
+}
+
+export default App;
